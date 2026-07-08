8dd52266fde51bc4fcf2e01d644c23b6783f2eca
Jamínteles Desus Ribeiro Moura <devjaminteles@gmail.com>
2026-07-08 11:27:28 -0300

    aa

--- src/api/routes/funcionario.routes.js
+++ src/api/routes/funcionario.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import * as controller from "../controllers/funcionario.controller.js"
+import * as controller from "../controllers/funcionario.controller.js" // casediference
 
 const router = Router()
 
